351621ad2ed45abaefbf4c8824d88d55e45b92c7
nividimka <leha.asd95@gmail.com>
2016-07-29 19:08:17 +0600

    add som

---
+++ app/src/main/java/biz/galamat/weatherapp/SettingsActivity.java
@@ -0,0 +1,13 @@
+package biz.galamat.weatherapp;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
