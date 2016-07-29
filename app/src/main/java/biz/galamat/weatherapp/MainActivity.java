938a10a34b2b93c34af080ea3abe84cc94b34143
Nividimka <leha.asd95@gmail.com>
2016-07-29 08:44:34 +0600

    Initial commit

---
+++ app/src/main/java/biz/galamat/weatherapp/MainActivity.java
@@ -0,0 +1,53 @@
+package biz.galamat.weatherapp;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends ActionBarActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().add(R.id.container,new PlaceholderFragment()).commit();
+        }
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.main,menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//        if (id = R.id.action_settings){
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+
+    public static class PlaceholderFragment extends Fragment {
+        public PlaceholderFragment(){
+
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main,null);
+            String[] items = {"Today - Sunny - 88 / 63","Tomorrow - Foggy - 70 / 46"};
+            return rootView;
+        }
+    }
+}
